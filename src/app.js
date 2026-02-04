0eae343a005a388f2ce384f1008a024af12f5b04
Micaela Camerini <micaelacamerini19@gmail.com>
2026-02-04 20:32:53 -0300

    instalacion y configuracion de swagger

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
 import "dotenv/config";
 import { connectMongoDB } from "./config/mongo.js";
 import express from "express";
@@ -8,6 +11,21 @@ const app = express();
 // Middleware para poder leer JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "API CANES",
+      description: "Documentación del proyecto final Backend III"
+    }
+  },
+  apis: ["./src/docs/*.yaml"]
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Ruta de prueba (temporal)
 app.get("/", (req, res) => {
